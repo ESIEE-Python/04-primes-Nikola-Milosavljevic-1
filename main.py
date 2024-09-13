6c9d5212bb4b058adf15f7d23b1fc905786a102c
Nikola-Milosavljevic-1 <nikola.milosavljevic@edu.esiee.fr>
2024-09-13 08:56:40 +0000

    Compte rendu nombre premier Milosavljevic Nikola

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
+"""c"""
 from math import sqrt
 
 #### Fonction secondaire
 
 
 def isprime(p):
+    """f"""
 
     # votre code ici
-
-    pass
+    pre=True
+    if p==1:
+        return False
+    for d in range (2,int(sqrt(p)+1)):
+        if p%d==0:
+            pre = False
+    return pre
 
 #### Fonction principale
 
 
 def main():
+    """h"""
 
     # vos appels à la fonction secondaire ici
 
-    for n in range(100):
+    for n in range(0, 100):
         if isprime(n):
             print(n, end=", ")
 
